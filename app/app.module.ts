17b159e427ed9b9c5cb3d7d675efbcd38539dfee
Leo Lanese <developer@leolanese.com>
2017-08-25 09:48:24 +0100

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule }      from '@angular/core';
-
 import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent }  from './app.component';
+import { FilterPipe} from './filter.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
-  bootstrap:    [ AppComponent ]
+  imports:[ BrowserModule, 
+            FormsModule 
+          ],
+  declarations:[ AppComponent, 
+                FilterPipe
+               ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
