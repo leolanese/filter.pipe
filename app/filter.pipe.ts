84f123b2a327453838826c17f63bcf39052e8bbd
Leo Lanese <developer@leolanese.com>
2017-08-25 10:42:04 +0100

    Update filter.pipe.ts

--- app/filter.pipe.ts
+++ app/filter.pipe.ts
@@ -4,16 +4,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   nameFilter: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+  // return a subset of an array of items if any item contains the searchText string
   transform(items: any[], searchText: string): any[] {
 
+    if (isString(value)){
+      throw new BaseException('Requires a String as input');
+    }
+    
+    // no string search
     if (!items) {
       return [];
     }
 
+    // no results
     if (!searchText) {
       return items;
     }
-
+    // to unify criteria
     searchText = searchText.toLowerCase();
 
     return items.filter( n => {
